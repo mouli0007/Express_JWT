35c682c44c34c521f438e8f084b0c356216d414e
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-05-01 19:59:52 +0530

    Create JWT_Authorization.js

---
+++ JWT_Authorization.js
@@ -0,0 +1,42 @@
+const JWT = require("jsonwebtoken");
+
+const check_auth = async (req, res, next) => {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(400).json({
+      errors: [
+        {
+          msg: "No token Found !",
+        },
+      ],
+    });
+  }
+
+  // Validating the token !
+
+  // It Validates the credetntials and provides u the payload !
+
+  try {
+    let user = await JWT.verify(token, "Our secret token !");
+    req.user = user.email;
+    next();
+  } catch (err) {
+    return res.status(400).json({
+      errors: [
+        {
+          msg: "Invalid Token ",
+        },
+      ],
+    });
+  }
+};
+
+module.exports.Authorize = check_auth;
+
+
+
+
+router.get("/private", Authorize, (req, res) => {
+  return res.status(200).json(private);
+});
