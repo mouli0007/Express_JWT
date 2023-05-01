3fc9b08a1f99a808959e877c83e39e1a5f27700c
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-05-01 14:23:30 +0530

    Update Express_Validators.js

--- Express_Validators.js
+++ Express_Validators.js
@@ -26,3 +26,49 @@ router.post(
 );
 
 module.exports = router;
+
+
+// Checking if the user is already exist !
+
+// Express Validator !
+const { check, validationResult } = require("express-validator");
+
+router.post(
+  "/signup",
+  [
+    // Making the checks
+
+    check("email", "Please Provide a Valid Email").isEmail,
+    check("password", "Please Provide a valid password !").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+
+    // Validate for thes user is its already exist!
+
+    let user = users.find((user) => {
+      return user.email === email;
+    });
+
+    if (user) {
+      return (
+        res.status(400),
+        json({
+          errors: [
+            {
+              msg: "Email  Already Exist ",
+            },
+          ],
+        })
+      );
+    }
+
+    return res.send(`Validation Passed for ${email}`);
+  }
+);
+
+module.exports = router;
